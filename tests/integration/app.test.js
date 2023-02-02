6af9e5302bb874bd4ddead42fab57dbf47c53b66
ronerog <ronerojr@gmail.com>
2023-02-02 10:19:18 -0300

    adicionando testes

--- tests/integration/app.test.js
+++ tests/integration/app.test.js
@@ -27,5 +27,30 @@ describe('Usando o método GET em /teams', function () {
           .get('/teams');
         expect(response.status).to.be.equal(200);
         expect(response.body.teams).to.be.deep.equal(output);
-    });
+    })
+  });
+
+describe('Usando o método POST', function () {
+  it('Retorna um time criado com um id', async function () {
+
+    const output = 
+      {
+        id: 3,
+        name: "Clube de Regatas do Flamengo",
+        initials: "FLA"
+      }
+    
+   
+      const response = await chai
+        .request(app)
+        .post('/teams')
+        .send({
+          id: 3,
+          name: "Clube de Regatas do Flamengo",
+          initials: "FLA"
+         });
+
+    expect(response.status).to.be.equal(201)
+    expect(response.body.team).to.be.deep.equal(output)
+  })
 });
